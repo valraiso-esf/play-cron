dd715afeb723ab0e22f096ac5e40eb70e7c56aac
Benoit <benoit.glevarec@valraiso.fr>
2017-10-26 13:32:49 +0200

    Better logs

--- src/main/scala/net/valraiso/CronTask.scala
+++ src/main/scala/net/valraiso/CronTask.scala
@@ -5,23 +5,24 @@ import java.util.{Timer, TimerTask}
 
 import com.github.buster84.cron.Schedule
 import org.joda.time._
-class CronTask(name: String, job: () => Unit, schedule: Schedule) {
+class CronTask(name: String, job: () => Any, schedule: Schedule) {
   private val log = Logger.getLogger("SchedulerLogger")
+  private def nowString() = DateTime.now().toString("dd/MM/yyyy hh:mm:ss.SSS")
   val timer = new Timer()
   add()
 
   private def add(): Unit = {
     timer.schedule(new TimerTask {
       override def run(): Unit = {
-        log.info(s"Running task $name at ${DateTime.now().toString("dd/MM/yyyy hh:mm:sss")}")
+        log.info(s"Running task $name at ${nowString()}")
         add()
         job()
       }
-    }, schedule.getNextAfter(DateTime.now()).toDate)
+    }, schedule.getNextAfter(DateTime.now().withSecondOfMinute(0).withMillisOfSecond(0)).toDate)
   }
 
   def cancel() = {
-    log.info(s"Stopping schedule of task $name at ${DateTime.now().toString("dd/MM/yyyy hh:mm:sss")}")
+    log.info(s"Stopping schedule of task $name at ${nowString()}")
     timer.cancel()
   }
 }
