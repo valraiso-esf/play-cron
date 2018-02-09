767a3e0907b7afde97acc4800c5af74554556268
Benoit <benoit.glevarec@valraiso.fr>
2018-02-09 09:22:15 +0100

    Add convenient methods

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := "play-cron"
 
 organization := "net.valraiso"
 
-version := "0.0.1"
+version := "0.0.2"
 
 scalaVersion := "2.11.7"
 
--- src/main/scala/net/valraiso/CronScheduler.scala
+++ src/main/scala/net/valraiso/CronScheduler.scala
@@ -9,10 +9,20 @@ class CronScheduler(){
   private val log = Logger.getLogger("SchedulerLogger")
   private val tasks = scala.collection.mutable.ArrayBuffer.empty[CronTask]
   log.info("Instantiate CronScheduler")
+
+
   def addTask(name: String, job: () => Unit, cronExpression: String, timezone: DateTimeZone = DateTimeZone.forID("Europe/Paris")) = {
     log.info(s"Adding task $name ($cronExpression)")
     val t = new CronTask(name, job, Schedule(cronExpression, timezone))
     tasks+=t
+  }
+
+  def addDaily(name: String, job: () => Unit, hour: Int, minutes: Int, timezone: DateTimeZone = DateTimeZone.forID("Europe/Paris")) = {
+    addTask(name, job, s"${minutes} ${hour} * * *")
+  }
+
+  def addHourly(name: String, job: () => Unit, minutes: Int, timezone: DateTimeZone = DateTimeZone.forID("Europe/Paris")) = {
+    addTask(name, job, s"${minutes} * * * *")
   }
 
   def stop() = {
